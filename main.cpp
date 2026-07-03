fecb73fee50fe44052d8ab2910b6f75508d5d2af
Soham-3004 <soham.deshmukh2020@gmail.com>
2026-07-03 12:38:20 +0000

    Implement complete CRUD REST API

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ struct Users{
 int main()
 {
     crow::SimpleApp app;
-
-    CROW_ROUTE(app, "/")    //lambda code [capture] (parameters) {  /*code*/ }
+    //simple get routes
+    CROW_ROUTE(app, "/")    //lambda code [capture] (parameters) {  /*code*/ }  
     ([]()   
     {
         return "Hello REST API\n";
     });
-
+    //Using JSON
     CROW_ROUTE(app, "/about")    //These are static routes
     ([]()   
     {
@@ -38,12 +38,12 @@ int main()
 
         return x;
     });    
-
+    //Maps to created a sorted in memory database
     std::map<int,Users> users;
     users[1] = {1, "Soham", "AI Engineer"};
     users[2] = {2, "Alex", "Backend Developer"};
     users[3] = {3, "John", "Data Scientist"};
-
+    //Search in database using GET
     CROW_ROUTE(app,"/users/<int>")
     ([&users](int id){
         auto it = users.find(id);
@@ -58,7 +58,7 @@ int main()
 
         return crow::response{x};
     });
-
+    //Returning entire database as an array of json
     CROW_ROUTE(app,"/users")
     ([&users](){
         crow::json::wvalue userJSON;
@@ -72,6 +72,54 @@ int main()
         }
         return userJSON;
     });
+    //Update or  POST method
+    int nextID = users.rbegin()->first+1;   //Server creates its own id aka primary key
+    CROW_ROUTE(app,"/users").methods(crow::HTTPMethod::Post)    //using post method
+    ([&users,&nextID](const crow::request& req){
+        auto body = crow::json::load(req.body);
+        if (!body){
+            return crow::response(400, "Invalid JSON");
+        }
+
+        Users user;
+        user.id = nextID++;
+        user.name = body["name"].s();
+        user.role = body["role"].s();
+        users[user.id] = user;
+
+        return crow::response(201);
+    });
+    //updating database with PUT which is a Idempotent method
+     CROW_ROUTE(app,"/users/<int>").methods(crow::HTTPMethod::Put)
+     ([&users](const crow::request& req, int id){
+        auto it = users.find(id);
+        if(it == users.end()){
+            return crow ::response(404,"User not found");
+        }
+
+        auto body = crow::json::load(req.body);
+        if(!body){
+            return crow::response(400, "Invalid JSON");
+        }
+
+        it->second.name = body["name"].s();
+        it->second.role = body["role"].s();
+        crow::json::wvalue x;
+        x["id"] = it->second.id;
+        x["name"] = it->second.name;
+        x["role"] = it->second.role;
+        return crow::response{x};
+     });
+
+     CROW_ROUTE(app,"/users/<int>").methods(crow::HTTPMethod::Delete)
+     ([&users](int id){
+        auto it = users.find(id);
+        if(it == users.end()){
+            return crow ::response(404,"User not found");
+        }
+        users.erase(it);
+        return crow::response(204, "User deleted successfully!");
+     });
 
     app.port(18080).multithreaded().run();
 }
