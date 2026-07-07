01bd97ffe2339a74adc257f0a2d68a4287365be9
Soham-3004 <soham.deshmukh2020@gmail.com>
2026-07-07 12:34:05 +0000

    Build frontend and integrate REST API with dynamic user management

--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,69 @@
 console.log("Script Started!");
-fetch("http://localhost:18080/users").then(response => {console.log(response);});
+
+const tableBody = document.getElementById("userTable");
+
+const nameInput = document.getElementById("nameInput");
+const roleInput = document.getElementById("roleInput");
+const addUserBtn = document.getElementById("addUserBtn");
+
+function fetchUsers(){
+    fetch("http://localhost:18080/users")
+    .then(response => response.json())
+    .then(data => {renderUsers(data)});
+}
+fetchUsers();
+
+function renderUsers(users){
+    tableBody.innerHTML = "";
+     for(const user of users){
+        createUserRow(user);
+     }
+}
+
+function createUserRow(user){
+        //create table row ele
+        const row = document.createElement("tr");
+        
+        //create table data
+        const idCell = document.createElement("td");
+        const nameCell = document.createElement("td");
+        const roleCell = document.createElement("td");
+
+        //add data
+        idCell.textContent = user.id;
+        nameCell.textContent = user.name;
+        roleCell.textContent = user.role;
+
+        //add the rows with data to table row
+        row.appendChild(idCell);
+        row.appendChild(nameCell);
+        row.appendChild(roleCell);  
+
+        //finally append each table row to the table body 
+        tableBody.appendChild(row);      
+}
+
+function addUser(user){
+    return fetch("http://localhost:18080/users", 
+        {method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body:JSON.stringify(user)
+        });
+}
+
+function handleAddUser(){
+    const name = nameInput.value;
+    const role = roleInput.value;
+    
+    const user = {
+        name,
+        role
+    };
+    addUser(user).then(() => {
+        fetchUsers();
+        nameInput.value = "";
+        roleInput.value = "";
+    });
+}
+
+addUserBtn.addEventListener("click",()=>{handleAddUser()});
--- include/cors_middleware.h
+++ include/cors_middleware.h
@@ -12,5 +12,7 @@ struct cors_middleware
     }
     void after_handle(crow::request& req, crow::response &res, context &ctx){
         res.add_header( "Access-Control-Allow-Origin","*");
+        res.add_header("Access-Control-Allow-Headers","Content-Type");
+        res.add_header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
     }
 };
